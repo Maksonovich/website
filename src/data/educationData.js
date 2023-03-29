a9e7c078be730fe1925e4615cb3fb2a5742059af
Maksonovich <kaverron1@gmail.com>
2023-03-29 16:49:10 +0600

    portfolio

--- src/data/educationData.js
+++ src/data/educationData.js
@@ -1,20 +1,6 @@
 export const educationData = [
     {
-        id: 1,
-        institution: 'Noakhali Government College',
-        course: 'Bachelor Degree',
-        startYear: '2020',
-        endYear: 'Present'
-    },
-    {
         id: 2,
-        institution: 'Noakhali Islamia Kamil Madrasah',
-        course: 'Higher Secondary',
-        startYear: '2018',
-        endYear: '2020'
-    },
-    {
-        id: 3,
         institution: 'School 2 , Kara-Balta',
         course: 'Secondary School',
         startYear: '2012',
--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -2,49 +2,32 @@ import eight from '../assets/svg/projects/eight.svg'
 import seven from '../assets/svg/projects/seven.svg'
 import six from '../assets/svg/projects/six.svg'
 
-
 export const projectsData = [
     {
         id: 1,
-        projectName: 'Ayla Networks',
-        projectDesc: 'This project about Simplifying the Development of Smart Home & IoT Solutions.',
-        tags: ['React', 'SAAS', 'React Bootstrap'],
+        projectName: 'ELEAN STORE',
+        projectDesc: 'THis project about women smokings , it`s online store where any women can buy for theme great smoking.',
+        tags: ['React', 'SAAS', 'Redux'],
         code: '',
-        demo: 'https://www.aylanetworks.com/',
+        demo: 'https://elean-store.vercel.app/',
         image: six
     },
     {
         id: 2,
-        projectName: 'Jewelry Niche',
-        projectDesc: 'This project is a Jewelry shop, Customer can be buy product and manage them and make payment.',
-        tags: ['React', 'Express', 'Material Ui'],
-        code: 'https://github.com/said7388/jewelry-niche',
-        demo: 'https://pandora-web.netlify.app/',
+        projectName: 'ALLEGRIA',
+        projectDesc: 'This project about wears.',
+        tags: ['javaScript', 'css', 'HTML'],
+        code: 'https://github.com/Maksonovich/Allegria',
+        demo: 'https://allegria.vercel.app/https://allegria.vercel.app/',
         image: seven
     },
     {
         id: 3,
-        projectName: 'Travel Agency',
-        projectDesc: 'This project about travel agency management.',
-        tags: ['React', 'CSS', 'Material Ui'],
-        code: 'https://github.com/said7388/Travel-Agency-client',
-        demo: 'https://travel-agency-fc58b.web.app/',
+        projectName: 'Quiz App',
+        projectDesc: 'This is Quiz App.',
+        tags: ['JavaScript', 'CSS', 'HTTP'],
+        code: 'https://github.com/Maksonovich/AppleS.git',
+        demo: 'https://apple-s.vercel.app/',
         image: eight
     }
 ]
-
-
-// Do not remove any fields.
-// Leave it blank instead as shown below
-
-/* 
-{
-    id: 1,
-    projectName: 'Car Pooling System',
-    projectDesc: '',
-    tags: ['Flutter', 'React'],
-    code: '',
-    demo: '',
-    image: ''
-}, 
-*/

6aee891fc6f3c7cd5f2f9f1dde23c1a3b035aa86
Maksonovich <kaverron1@gmail.com>
2023-03-30 18:44:05 +0600

    some change

--- src/data/educationData.js
+++ src/data/educationData.js
@@ -1,9 +1,9 @@
 export const educationData = [
     {
-        id: 2,
-        institution: 'School 2 , Kara-Balta',
-        course: 'Secondary School',
-        startYear: '2012',
+        id: 1,
+        institution: 'Kyrgyzstan,Kara-Balta , it courses Geeks',
+        course: 'Education Centre GEEKS',
+        startYear: '2022',
         endYear: '2023'
-    },
+    }
 ]
